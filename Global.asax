0c8b28ceafd10d503b69491d69af2c0c7d0a4b52
Shawno0 <dev@sizorax.com>
2021-05-07 08:47:55 +0100

    Initial Commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PanelLayoutTest.MvcApplication" Language="C#" %>
---
+++ Scripts/Panels.js
@@ -0,0 +1,78 @@
+﻿var activePanel = null;
+var selectedPanel = null;
+
+$(document).ready(function () {
+    var gpanel = null;
+
+    $(document.body).on("mousemove", function (e) {
+        if (gpanel) {
+            PauseMouseEvents(e);
+            gpanel.style.top = e.clientY + 'px';
+            gpanel.style.left = e.clientX + 'px';
+        }
+    });
+
+
+    $(document.body).on("mousedown", ".Content", function (e) {
+        if (e.target.classList.contains('Header')) {
+            gpanel = e.target.parentElement;
+            gpanel.style.top = e.clientY + 'px';
+            gpanel.style.left = e.clientX + 'px';
+            gpanel.classList.add('Moving');
+        }
+    });
+
+    $(document.body).on("mouseup", function (e) {
+        if (gpanel != null) {
+            gpanel.classList.remove('Moving');
+            gpanel.style.top = null;
+            gpanel.style.left = null;
+
+            UpdatePanelPosition(gpanel.parentElement);
+        }
+        gpanel = null;
+    });
+});
+
+function SelectPanel(id) {
+    var panel = document.getElementById(id);
+    if (panel.classList.contains("GPanel") && !panel.classList.contains('Static')) {
+        selectedPanel = panel;
+    }
+}
+
+function UpdatePanelPosition(panel) {
+    if (selectedPanel != null) {
+        var newGridPos = GetGridPositions(selectedPanel.classList);
+        var oldGridPos = GetGridPositions(panel.classList);
+
+        panel.className = 'GPanel ' + newGridPos;
+        selectedPanel.className = 'GPanel ' + oldGridPos;
+    }
+}
+
+function GetGridPositions(cList) {
+    var gridPos = '';
+    if (cList.length != undefined && cList.length > 1) {
+        for (var i = 0; i < cList.length; i++) {
+            if (cList[i] != 'GPanel') {
+                if (gridPos.length == 0) {
+                    gridPos += cList[i];
+                }
+                else {
+                    gridPos += ' ' + cList[i];
+                }
+            }
+        }
+    }
+
+    return gridPos;
+}
+
+function PauseMouseEvents(e) {
+    if (e.stopPropagation) e.stopPropagation();
+    if (e.preventDefault) e.preventDefault();
+    e.cancelBubble = true;
+    e.returnValue = false;
+    return false;
+}
